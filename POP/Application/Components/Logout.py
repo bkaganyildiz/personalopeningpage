9353e7a24d50e87699960cc1ffb019581cd3b789
Kagan <bkaganyildiz@github.com>
2017-01-12 10:00:12 +0200

    New Logout Component

---
+++ POP/Application/Components/Logout.py
@@ -0,0 +1,47 @@
+from dominate.tags import *
+import json
+
+
+class Login(object):
+    _description = "Logout Page"
+    _attributes = [
+    ]
+    _methods = [
+    ]
+
+    def description(self):
+        return self._description
+
+    def attributes(self):
+        return self._attributes
+
+    def __getitem__(self, item):
+        for attribute in self._attributes:
+            if attribute[0] == item:
+                return getattr(self, item)
+
+        raise AttributeError(self.__class__.__name__ + " object has no attribute '" + item + "'")
+
+    def __setitem__(self, key, value):
+        for attribute in self._attributes:
+            if attribute[0] == key:
+                setattr(self, key, value)
+                return
+
+        raise AttributeError(self.__class__.__name__ + " object has no attribute '" + key + "'")
+
+    def methods(self):
+        return self._methods
+
+    def __str__(self):
+        return self.description()
+
+    def __init__(self, username="", password=""):
+        self.username = username
+        self.password = password
+
+    def execute(self):
+        h = div(
+            h1("Successfuly Logout")
+        )
+        return h
